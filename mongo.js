680d333f65097ad3c5dae0ba9862d6beee1341af
mateuszbaszaraba <64064788+mateuszbaszaraba@users.noreply.github.com>
2021-08-31 16:26:57 +0200

    login

--- mongo.js
+++ mongo.js
@@ -32,14 +32,14 @@ const note = new Note({
   important: true,
 });
 
-// note.save().then((result) => {
-//   console.log('note saved!');
-//   mongoose.connection.close();
-// });
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+note.save().then((result) => {
+  console.log('note saved!');
   mongoose.connection.close();
 });
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
